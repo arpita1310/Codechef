97b314c43461c5d22a60fa5b092467e37be9b241
Arpita Singh <arpitaswork@gmail.com>
2023-05-09 00:28:10 +0530

    Determine Score

---
+++ DetermineScore.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int test;
+    cin>>test;
+    int x,y;
+    for(int i=0;i<test;i++){
+        cin>>x>>y;
+        cout<<(x*y)/10<<endl;
+    }
+}
