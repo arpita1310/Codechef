57a2731c9412c0382f44c7594457d03ee76a115a
Arpita Singh <105579731+arpita1310@users.noreply.github.com>
2023-06-19 23:51:04 +0530

    Add files via upload

---
+++ ODDPAIRS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    int cnt=0;
+	   cout<<(n*n)/2<<endl;
+	}
+	return 0;
+}
