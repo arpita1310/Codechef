e324788f963bf4a13828514aaab4162e8252bbc0
Arpita Singh <105579731+arpita1310@users.noreply.github.com>
2023-06-21 23:27:22 +0530

    Add files via upload

---
+++ MAGICHF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int n, x, s, a, b;
+	    cin>> n >> x >> s;
+
+        for (int i=0; i<s; i++) {
+            cin>> a >> b;
+            if (x==a) x = b;
+            else if (x==b) x = a;
+        }
+        cout<< x <<endl;
+	}
+	return 0;
+}
